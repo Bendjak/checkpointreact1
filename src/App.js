edd0ef2062b2749a86948880fdd89394708f7c24
Bendjak <akodidesireakoussi@gmail.com>
2025-02-17 01:20:39 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Card, Container } from 'react-bootstrap';
+import Name from './components/Name';
+import Price from './components/Price';
+import Description from './components/Description';
+import Image from './components/Image';
+
+const firstName = "Desire"; // Remplacez par votre prénom ou laissez vide pour tester les deux cas
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="mt-5">
+      <Card style={{ width: '18rem' }}>
+        <Image />
+        <Card.Body>
+          <Name />
+          <Price />
+          <Description />
+        </Card.Body>
+      </Card>
+      <h2 className="mt-4">
+        Bonjour, {firstName ? firstName : '!' }
+      </h2>
+      {firstName && (
+        <img 
+          src="montre.png" 
+          alt="profile" 
+          className="rounded-circle mt-3" 
+        />
+      )}
+    </Container>
   );
 }
 
 export default App;
+
---
+++ src/product.js
@@ -0,0 +1,9 @@
+const product = {
+    name: "Montre Élégante",
+    price: "2000 €",
+    description: "Une montre élégante pour toutes les occasions.",
+    imageUrl: process.env.PUBLIC_URL + "montre.png"
+  };
+  
+  export default product;
+  
